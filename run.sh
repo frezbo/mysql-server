f9f9bb1d316d3cd82970a64eb348f8175b8847dc
Noel Georgi <noel.georgi@reancloud.com>
2017-01-16 08:11:14 +0530

    added condition checks

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 MYSQL_DIR=/var/lib/mysql
-import_file=$(mktemp)
 
 if [ ! -d "$MYSQL_DIR/mysql" ]; then
+	import_file=$(mktemp)
 	mysql_install_db --user=root > /dev/null 2>&1 || echo "Database initialization failed"
 	if [ ! -z "$MYSQL_ROOT_PASS" ]; then
 		cat << EOF > $import_file
@@ -29,6 +29,9 @@ EOF
 	fi
 /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $import_file > /dev/null 2>&1 || echo "Database creation failed"
 rm -f "$import_file"
+elif [ ! -z "$MYSQL_ROOT_PASS" -o ! -z "$MYSQL_DB_NAME" -o ! -z "$MYSQL_DB_USER" -o ! -z "$MYSQL_DB_PASS" ]; then
+	echo "Arguments should not be provided while running the container as daemon"
+	exit 1
 else
 	mkdir -p /run/mysqld
 	exec /usr/bin/mysqld --user=root --console 
